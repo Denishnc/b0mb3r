5cd36e1739584b32ce9d2c75859d8a7570c8e892
Mark <isfellop@gmail.com>
2019-07-20 09:51:54 +0300

    +2

---
+++ services/newnext.py
@@ -0,0 +1,8 @@
+from service import Service
+
+
+class NewNext(Service):
+    def send_sms(self):
+        self.session.post('https://newnext.ru/graphql', json={'operationName': 'registration', 'variables': {
+            'client': {'firstName': 'Иван', 'lastName': 'Иванов', 'phone': self.phone, 'typeKeys': ['Unemployed']}},
+                                                              'query': 'mutation registration($client: ClientInput!) {\n  registration(client: $client) {\n    token\n    __typename\n  }\n}\n'})
