755349e722a2397fa55817f9bf478700683fcadd
Mark <isfellop@gmail.com>
2019-07-20 15:53:22 +0300

    +1

---
+++ services/tanuki.py
@@ -0,0 +1,10 @@
+from service import Service
+
+
+class Tanuki(Service):
+    def send_sms(self):
+        phone = f'(+{self.phone[0]}){self.phone[1:11]}'
+        self.session.post('https://www.tanuki.ru/api/', json={
+            'header': {'version': '2.0', 'userId': '1', 'debugMode': True,
+                       'agent': {'device': 'desktop', 'version': '1'}, 'langId': '1', 'cityId': '1', 'dbgValue': ''},
+            'method': {'name': 'sendSmsCode'}, 'data': {'phone': phone, 'type': 1}})
